1142c435e6c369d3dbbbad431dfb18b3c3c376f2
amjayy <33906054+amjayy@users.noreply.github.com>
2018-01-29 16:16:22 -0600

    Add files via upload

---
+++ lp3thw_pt.2/Exercise # 28/exercise_28.py
@@ -0,0 +1,52 @@
+#Guess: This will be True.
+True and True
+
+#Guess: this will be false.
+False and True
+
+# Guess: This will be True.
+1 == 1 or 2 ==1
+
+# Guess: This will be True.
+"test" == "test"
+
+
+#Guess: This will be True
+1==1 or 2 != 1
+
+#Guess: This will be True.
+True and 1==1
+
+#Guess: This will be False.
+False  and 0 != 0
+
+#Guess:This will be True
+True or 1 ==1
+
+# Guess: This will be False.
+"test" == "testing"
+
+#Guess: This will be False.
+1 != 0 and 2==1
+
+#Guess: This will be True.
+"test" != "testing"
+
+#Guess: This willl be True.
+"test" == 1
+
+#Guess:This will be False.
+not(True and False)
+
+# Guess: This will be False.
+not(1 ==1 and 0 != 1)
+
+#Guess: This will be True.
+not (10 == 1 or 1000 == 1000)
+
+#Guess: This will be
+
+
+
+
+
